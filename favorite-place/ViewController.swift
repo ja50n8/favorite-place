ea6d89afc46255f094a3da70e5e6e6d7c0bf8c60
Jason Bell <ja50n8@yahoo.co.uk>
2016-07-20 10:15:29 +0100

    Initial Commit

---
+++ favorite-place/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  favorite-place
+//
+//  Created by Jason Bell on 20/07/2016.
+//  Copyright © 2016 Cold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
