73db54efa220eb97644172ef9720ef7585dbf74f
Jason Bell <ja50n8@yahoo.co.uk>
2016-08-15 13:10:43 +0100

    iOS Course

---
+++ favorite-place/DetailVCViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailVCViewController.swift
+//  favorite-place
+//
+//  Created by Jason Bell on 20/07/2016.
+//  Copyright © 2016 Cold. All rights reserved.
+//
+
+import UIKit
+
+class DetailVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
